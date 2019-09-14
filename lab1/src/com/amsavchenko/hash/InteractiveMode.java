3195055de7c7a4ce59d50312b28579b5699685a8
Anton <savchenko_a@icloud.com>
2019-09-14 22:38:03 +0300

    v-1

---
+++ lab1/src/com/amsavchenko/hash/InteractiveMode.java
@@ -0,0 +1,65 @@
+package com.amsavchenko.hash;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InteractiveMode
+{
+	String[] args;
+	boolean userWantToStop;
+
+	public InteractiveMode (String[] input)
+	{
+		this.args = input;
+		this.userWantToStop = false;
+	}
+
+	void printHash (String fileName)
+	{
+		try
+		{
+			CountHash ch = new CountHash(fileName);
+			String md5 = ch.countMd5();
+			String sha256 = ch.countSha256();
+			System.out.println("\nFile : " + fileName + "\nMd5 : " + md5 + 
+				"\nSha256 : " + sha256 + "\n");
+		}
+		catch (IOException e)
+		{
+			System.out.println("File does not found!\n");
+		}
+			
+	}
+
+	void interactiveTalk ()
+	{
+		while (! userWantToStop)
+		{
+			System.out.println("Enter file / files names :");
+			Scanner in = new Scanner(System.in);
+			for (String str : in.nextLine().split(" "))
+				printHash(str);
+
+			boolean badAnswer = true;
+			while (badAnswer)
+			{
+				System.out.println("Do you want to count hash of another files?\n1 - Yes\n2 - No");
+				String response = in.next();
+				if (response.equals("2"))
+				{
+					userWantToStop = true;
+					badAnswer = false;
+				}
+				else if (response.equals("1")) 
+				{
+					badAnswer = false;
+				}
+
+				else
+					System.out.println("Incorrect answer! Try again");
+			}
+		}
+	}
+
+
+}
---
+++ lab1/src/com/amsavchenko/hash/Main.java
@@ -0,0 +1,22 @@
+package com.amsavchenko.hash;
+
+
+public class Main 
+{
+	public static void main (String[] args) 
+	{
+
+		if (args[0].equals("-i"))
+		{
+			InteractiveMode im = new InteractiveMode(args);
+			im.interactiveTalk();
+
+		}
+		else
+		{
+			SimpleMode sm = new SimpleMode(args);
+			sm.countHash();
+		}
+		
+	}
+}
